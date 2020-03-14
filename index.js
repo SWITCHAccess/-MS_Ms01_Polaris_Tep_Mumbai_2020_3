65c8c27fc9089153f49b06ec6977c2308bcec4aa
roshan <roshanbhuvad15@gmail.com>
2020-03-14 18:32:15 +0530

    Assigment has been complete

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let user_1 = Math.floor(Math.random() * 30); 
+let user_2 = Math.floor(Math.random() * 30); 
+let user_3 = Math.floor(Math.random() * 30); 
+ 
+var total = user_1 + user_2 + user_3;
+console.log(total);
