75d78a23706bf5c7177f31c4966e041ab6f03fe1
roshan <roshanbhuvad15@gmail.com>
2020-03-16 22:15:19 +0530

    Adding New categories

--- index.js
+++
@@ -1,7 +0,0 @@
-
-let user_1 = Math.floor(Math.random() * 30); 
-let user_2 = Math.floor(Math.random() * 30); 
-let user_3 = Math.floor(Math.random() * 30); 
- 
-var total = user_1 + user_2 + user_3;
-console.log(total);
--- release1index.js
+++
@@ -1,6 +0,0 @@
-let user_1 = ["Apple","Pineapple","Guava","Raspberry","app","mang","Pine","ras","dry","wet","gua"];
-let user_2 = ["Mango", "Gooseberry","raspberry","blackberry","orange","letchy","pineapp","dragonfruit"];
-let user_3 = ["papaya","kiwi","khattmitta","pears","kairee","sweeten","dragon","banana","greenapple","ripe","nuts"];
-user_1 = user_1.concat(user_2);
-user_1 = user_1.concat(user_3);
-console.log(user_1);
